2bbc6a7dfeb65b37fbee5b3d311c3a39dfb10848
lizmery <l.vigil096@gmail.com>
2024-06-25 14:47:01 -0700

    test download link

--- src/app/account/stripe/purchase-success/page.tsx
+++ src/app/account/stripe/purchase-success/page.tsx
@@ -52,7 +52,7 @@ export default async function SuccessPage({
                             await createDownloadVerification(product.id) ? (
 
                             <a href={product.fileDownLink}>
-                                Download - {product.fileDownLink}
+                               *TEST* Download - {product.fileDownLink}
                             </a> ) :  notFound()
                             // <p>test</p>
                         ) : (
